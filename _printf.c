be725176959bf80aee3f81cc04b12cf6c3176096
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-25 19:30:29 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,90 +9,90 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int printed_chars = 0;
+	va_list args;
+	int printed_chars = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
+	    while (*format != '\0')
+	    {
+		if (*format == '%')
+		{
+		    format++;
 
-            if (*format == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(null)";
-                while (*s != '\0')
-                {
-                    putchar(*s);
-                    s++;
-                    printed_chars++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                printed_chars++;
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(args, int);
-                int negative = 0;
+		    if (*format == 'c')
+		    {
+			int c = va_arg(args, int);
+			putchar(c);
+			printed_chars++;
+		    }
+		    else if (*format == 's')
+		    {
+			char *s = va_arg(args, char *);
+			if (s == NULL)
+			    s = "(null)";
+			while (*s != '\0')
+			{
+			    putchar(*s);
+			    s++;
+			    printed_chars++;
+			}
+		    }
+		    else if (*format == '%')
+		    {
+			putchar('%');
+			printed_chars++;
+		    }
+		    else if (*format == 'd' || *format == 'i')
+		    {
+			int num = va_arg(args, int);
+			int negative = 0;
 
-                if (num < 0)
-                {
-                    negative = 1;
-                    num = -num;
-                }
+			if (num < 0)
+			{
+			    negative = 1;
+			    num = -num;
+			}
 
-                int num_digits = 0;
-                int num_copy = num;
-                do
-                {
-                    num_digits++;
-                    num_copy /= 10;
-                } while (num_copy != 0);
+			int num_digits = 0;
+			int num_copy = num;
+			do
+			{
+			    num_digits++;
+			    num_copy /= 10;
+			} while (num_copy != 0);
 
-                if (negative)
-                {
-                    putchar('-');
-                    printed_chars++;
-                }
+			if (negative)
+			{
+			    putchar('-');
+			    printed_chars++;
+			}
 
-                do
-                {
-                    int digit = num % 10;
-                    putchar('0' + digit);
-                    printed_chars++;
-                    num /= 10;
-                } while (num != 0);
+			 do
+			{
+			    int digit = num % 10;
+			    putchar('0' + digit);
+			    printed_chars++;
+			    num /= 10;
+			} while (num != 0);
 
-            }
-            else
-            {
-                putchar('%');
-                putchar(*format);
-                printed_chars += 2;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            printed_chars++;
-        }
-        format++;
-    }
+		    }
+		    else
+		    {
+			putchar('%');
+			putchar(*format);
+			printed_chars += 2;
+		    }
+		}
+		else
+		{
+		    putchar(*format);
+		    printed_chars++;
+		}
+		format++;
+	    }
 
-    va_end(args);
+	    va_end(args);
 
-    return (printed_chars);
-}
+	    return (printed_chars);
+	}
