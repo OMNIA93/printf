20e9da27a75ea97765667010405269e76487543f
Haneenmohamed8 <haneenmuhammed181@gmail.com>
2023-04-25 15:21:58 -0700

    m

--- _printf.c
+++ _printf.c
@@ -1,1 +1,50 @@
-..
+#include "main.h"
+
+/**
+ * _printf - prints output according to a format
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    int len = 0; /* length of printed string */
+    va_list args; /* argument list */
+    char c;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    c = (char) va_arg(args, int);
+                    len += _putchar(c);
+                    break;
+                case 's':
+                    len += _puts(va_arg(args, char *));
+                    break;
+                case '%':
+                    len += _putchar('%');
+                    break;
+                default:
+                    len += _putchar('%');
+                    len += _putchar(*format);
+                    break;
+            }
+        }
+        else
+        {
+            len += _putchar(*format);
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return (len);
+}
