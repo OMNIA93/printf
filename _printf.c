e7ee1d4f1c97da3ae2676b17e3140db31ffdaec5
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-25 17:41:15 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,54 +1,41 @@
 #include <main.h>
 
 /**
- * _putchar - writes a character to stdout
- * @c: the character to write
+ * _printf - ......
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+int _printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    int count = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == 'c') {
+                char c = (char) va_arg(args, int);
+                putchar(c);
+                count++;
+            } else if (*format == 's') {
+                char *str = va_arg(args, char*);
+                while (*str != '\0') {
+                    putchar(*str);
+                    str++;
+                    count++;
+                }
+            } else if (*format == '%') {
+                putchar('%');
+                count++;
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
-
-/**
- * _printf - produces output according to a format
- * @format: a character string
- *
- * Return: the number of characters printed (excluding the null byte used to end output to strings)
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				while (*s)
-				{
-					_putchar(*s);
-					count++;
-					s++;
-				}
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-		}
