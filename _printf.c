8f0939ad99a6dbae971f67d6fcc3cebbc1c1f65d
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-25 07:17:43 +0200

    update

--- _printf.c
+++ _printf.c
@@ -1,39 +1,55 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int count = 0;
-    char c;
-    while ((c = *format++) != '') {
-        if (c == '%') {
-            switch (*format++) {
-                case 'c': {
-                    int arg = va_arg(args, int);
-                    putchar(arg);
-                    count++;
-                    break;
-                }
-                case 's': {
-                    char *arg = va_arg(args, char *);
-                    while (*arg != '') {
-                        putchar(*arg++);
-                        count++;
-                    }
-                    break;
-                }
-                case '%': {
-                    putchar('%');
-                    count++;
-                    break;
-                }
-            }
-        } else {
-            putchar(c);
-            count++;
-        }
-    }
-    va_end(args);
-    return count;
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to write
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
+
+/**
+ * _printf - produces output according to a format
+ * @format: a character string
+ *
+ * Return: the number of characters printed (excluding the null byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+				_putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				while (*s)
+				{
+					_putchar(*s);
+					count++;
+					s++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
