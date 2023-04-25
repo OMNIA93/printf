06dce479bb06ed0d7598dd8f6fb5db102d80dc30
Haneenmohamed8 <haneenmuhammed181@gmail.com>
2023-04-25 15:26:08 -0700

    m

--- _printf.c
+++ _printf.c
@@ -48,3 +48,17 @@ int _printf(const char *format, ...)
 
     return (len);
 }
+
+
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
