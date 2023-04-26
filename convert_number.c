107a4151a2f4e9d7807a3e956a02a385191a9486
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-26 05:04:21 +0200

    Update convert_number.c

--- convert_number.c
+++ convert_number.c
@@ -1,106 +1,130 @@
 #include "main.h"
 
 /**
- * print_hex - prints unsigned hex numbers in lowercase
- * @ap: the argument pointer
- * @params: the parameters struct
+ * _isdigit - checks if character is digit
+ * @c: the character to check
  *
- * Return: bytes printed
+ * Return: 1 if digit, 0 otherwise
  */
-int print_hex(va_list ap, params_t *params)
+int _isdigit(int c)
 {
-	unsigned long l;
-	int c = 0;
-	char *str;
+	return (c >= '0' && c <= '9');
+}
 
-	switch (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
-	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
-	else
-		l = (unsigned int)va_arg(ap, unsigned int);
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
-	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
-	switch (params->hashtag_flag && l)
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_number - prints a number with options
+ * @str: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number(char *str, params_t *params)
+{
+	unsigned int i = _strlen(str);
+	int neg = (!params->unsign && *str == '-');
+
+	if (!params->precision && *str == '0' && !str[1])
+		str = "";
+	if (neg)
 	{
-		*--str = 'x';
-		*--str = '0';
+		str++;
+		i--;
 	}
-	params->unsign = 1;
-	return (c += print_number(str, params));
-}
+	if (params->precision != UINT_MAX)
+		while (i++ < params->precision)
+			*--str = '0';
+	if (neg)
+		*--str = '-';
 
-/**
- * print_HEX - prints unsigned hex numbers in uppercase
- * @ap: the argument pointer
- * @params: the parameters struct
- *
- * Return: bytes printed
- */
-int print_HEX(va_list ap, params_t *params)
-{
-	unsigned long l;
-	int c = 0;
-	char *str;
-
-	switch (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
-	else if (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+	if (!params->minus_flag)
+		return (print_number_right_shift(str, params));
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
-
-	str = convert(l, 16, CONVERT_UNSIGNED, params);
-	switch (params->hashtag_flag && l)
-	{
-		*--str = 'X';
-		*--str = '0';
-	}
-	params->unsign = 1;
-	return (c += print_number(str, params));
-}
-/**
- * print_binary - prints unsigned binary number
- * @ap: the argument pointer
- * @params: the parameters struct
- *
- * Return: bytes printed
- */
-int print_binary(va_list ap, params_t *params)
-{
-	unsigned int n = va_arg(ap, unsigned int);
-	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
-	int c = 0;
-
-	switch (params->hashtag_flag && n)
-		* --str = '0' ;
-	params->unsign = 1;
-	return (c += print_number(str, params));
+		return (print_number_left_shift(str, params));
 }
 
 /**
- * print_octal - prints unsigned octal numbers
- * @ap: the argument pointer
- * @params: the parameters struct
+ * print_number_right_shift - prints a number with options
+ * @str: the base number as a string
+ * @params: the parameter struct
  *
- * Return: bytes printed
+ * Return: chars printed
  */
-int print_octal(va_list ap, params_t *params)
+int print_number_right_shift(char *str, params_t *params)
 {
-	unsigned long l;
-	char *str;
-	int c = 0;
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
 
-	switch (params->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
-	else if  (params->h_modifier)
-		l = (unsigned short int)va_arg(ap, unsigned int);
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
 	else
-		l = (unsigned int)va_arg(ap, unsigned int);
-	str = convert(l, 8, CONVERT_UNSIGNED, params);
+		neg = 0;
+	if ((params->plus_flag && !neg2) ||
+		(!params->plus_flag && params->space_flag && !neg2))
+		i++;
+	if (neg && pad_char == '0')
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+		!params->unsign && params->zero_flag)
+		n += _putchar(' ');
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	if (neg && pad_char == ' ')
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && params->space_flag && !neg2 &&
+		!params->unsign && !params->zero_flag)
+		n += _putchar(' ');
+	n += _puts(str);
+	return (n);
+}
 
-	switch (params->hashtag_flag && l)
-		* --str = '0' ;
-	params->unsign = 1;
-	return (c += print_number(str, params));
+/**
+ * print_number_left_shift - prints a number with options
+ * @str: the base number as a string
+ * @params: the parameter struct
+ *
+ * Return: chars printed
+ */
+int print_number_left_shift(char *str, params_t *params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = ' ';
+
+	if (params->zero_flag && !params->minus_flag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-');
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
+	else
+		neg = 0;
+
+	if (params->plus_flag && !neg2 && !params->unsign)
+		n += _putchar('+'), i++;
+	else if (params->space_flag && !neg2 && !params->unsign)
+		n += _putchar(' '), i++;
+	n += _puts(str);
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	return (n);
 }
