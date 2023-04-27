8980e154ecf1c981811801ba45e9ba2f5436498d
Haneenmohamed8 <haneenmuhammed181@gmail.com>
2023-04-26 21:37:54 -0700

    task0

--- _printf.c
+++ _printf.c
@@ -1,49 +1,51 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
+ * _printf - produces output according to a format.
+ * @format: character string
  *
- * Return: number of bytes printed
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	va_list args;
+	int i = 0, j = 0, count = 0;
+	char *str;
 
-	va_start(ap, format);
+	va_start(args, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = (char *)format; *p; p++)
+	while (format && format[i])
 	{
-		init_params(&params, ap);
-		if (*p != '%')
+		if (format[i] == '%' && format[i + 1])
 		{
-			sum += _putchar(*p);
-			continue;
+			i++;
+
+			switch (format[i])
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					if (str)
+					{
+						count += _puts(str);
+						break;
+					}
+					count += _puts("(null)");
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(format[i]);
+			}
 		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
-			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			sum += get_print_func(p, ap, &params);
+			count += _putchar(format[i]);
+		i++;
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	va_end(args);
+	return (count);
 }
