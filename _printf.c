9c30512a4d6eb5af3500a4e7158e0755b674b74c
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-27 07:07:35 +0200

    task0

--- _printf.c
+++ _printf.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
  * _printf - produces output according to a format.
@@ -48,4 +50,22 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
+}
+
+/**
+ * _strlen - Computes the length of a string
+ * @s: The string to compute the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+
+    return len;
 }
