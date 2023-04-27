b29c0703428e36b280422f392324de8caf116f59
OMNIA93 <omniannnnnnn@gmail.com>
2023-04-27 07:34:33 -0700

    v

--- _printf.c
+++ _printf.c
@@ -1,37 +1,41 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - printf implementation.
+ * @format: string to be printed
+ *
+ * Return: no. of printed characters
  */
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
+	print_t placeholders[] = {
 		{"c", print_char},
-		{"s", print_string},
+		{"s", print_str},
 		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
+		{"d", print_int},
+		{"i", print_int},
 		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
 		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
+		{"u", print_unsigned},
+		{"S", print_str_hex},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"p", print_ptr},
+		{"r", print_rev},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
-	va_list arg_list;
+	va_list list;
+	int char_num = 0;
+	char buffer[BUFFER_SIZE];
+
+	va_start(list, format);
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(arg_list, format);
-	/*Calling parser function*/
-	printed_chars = parser(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	char_num = help_print(format, list, placeholders, buffer);
+	va_end(list);
+
+	return (char_num);
 }
