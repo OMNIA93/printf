583541775ad5206ddd27a718ed9f6df96e568793
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-28 05:16:17 +0200

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,172 +1,107 @@
 #include "main.h"
 
 /**
- * print_char - print a character
- * @list: pointer to argument
- * @buffer: buffer to store char
- * @buff_pos: index
- * @flag: flag
- * @length: length
- * @width: width
- * Return: 1
+ * print_nonprintable - Non printable characters
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * 
+ * Return: number of char printed
  */
-int print_char(va_list list, char buffer[], int buff_pos, int flag,
-		int length, int width)
+int print_nonprintable(va_list l, flags_t *f)
 {
-	char c = va_arg(list, int);
-	int i = 0;
+	int i, count = 0;
+	char *res;
+	char *s = va_arg(l, char *);
 
-	UNUSED(flag);
-	UNUSED(length);
+	(void)f;
+	if (!s)
+		return (_puts("(null)"));
 
-	if (width > 1)
+	for (i = 0; s[i]; i++)
 	{
-		for (i = 0; i < (width - 1); i++)
-			buffer[buff_pos++] = ' ';
+		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			res = convert(s[i], 16, 0);
+			if (!res[1])
+				count += _putchar('0');
+			count += _puts(res);
+		}
+		else
+			count += _putchar(s[i]);
 	}
-	buffer[buff_pos++] = c;
-	i++;
+	return (count);
+}
+
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
+
+
+/**
+ * print_rev - prints a string in reverse
+ * @l: argument from _printf
+ * @f: pointer to the struct flags that determines
+ *
+ * Return: length of the printed string
+ */
+int print_rev(va_list l, flags_t *f)
+{
+	int i = 0, j;
+	char *s = va_arg(l, char *);
+
+	(void)f;
+	if (!s)
+		s = "(null)";
+
+	// get length of string
+	while (s[i])
+		i++;
+
+	// print string in reverse
+	for (j = i; j > 0; j--)
+		_putchar(s[j - 1]);
 
 	return (i);
 }
 
 /**
- * print_str - print a string.
- * @list: string to be printed
- * @buffer: buffer to store string
- * @buff_pos: index
- * @flag: flag
- * @width: width
- * @length: length
- * Return: no. of printed characters
+ * print_rot13 - prints a string using rot13
+ * @l: list of arguments from _printf
+ * @f: pointer to the struct flags that determines
+ * 
+ * Return: length of the printed string
  */
-int print_str(va_list list, char buffer[], int buff_pos, int flag,
-		int length, int width)
+int print_rot13(va_list l, flags_t *f)
 {
-	int i, len, j = 0;
-	char *str = va_arg(list, char *);
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(l, char *);
 
-	UNUSED(flag);
-	UNUSED(length);
-
-	if (str == NULL)
-		str = "(null)";
-	len = _strlen(str);
-	if (width > len)
-		for (j = 0; j < (width - len); j++)
-			buffer[buff_pos++] = ' ';
-
-	for (i = 0; str[i]; i++)
-		buffer[buff_pos++] = str[i];
-
-	return (len + j);
-}
-
-/**
- * print_percent - print % symbol
- * @list: % symbol to be printed
- * @buffer: buffer
- * @buff_pos: index
- * @flag: flag
- * @length: length
- * @width: width
- * Return: 1
- */
-int print_percent(va_list list, char buffer[], int buff_pos,
-		  int flag, int length, int width)
-{
-	UNUSED(list);
-	UNUSED(flag);
-	UNUSED(length);
-	UNUSED(width);
-
-	buffer[buff_pos] = '%';
-	return (1);
-}
-
-/**
- * print_int - print integer numbers
- * @list: number to be printed.
- * @buffer: buffer
- * @buff_pos: index
- * @flag: flag
- * @length: length
- * @width: width
- * Return: no. of printed digits
- */
-int print_int(va_list list, char buffer[], int buff_pos, int flag, int length,
-		int width)
-{
-	int digits = 0, i, j = 0;
-	long int num;
-	char number[11];
-
-	if (length == LONG)
-		num = va_arg(list, long int);
-	else if (length == SHORT)
-		num = (short)va_arg(list, int);
-	else
-		num = va_arg(list, int);
-
-	if (num < 0)
+	(void)f;
+	for (i = 0; s[i]; i++)
 	{
-		number[digits++] = '-';
-		num *= -1;
+		if (s[i] < 'A' || (s[i] > 'Z' && s[i] < 'a') || s[i] > 'z')
+			_putchar(s[i]);
+		else
+		{
+			char *rot13_ptr = strchr(rot13, s[i]);
+			if (rot13_ptr != NULL)
+				_putchar(ROT13[rot13_ptr - rot13]);
+		}
 	}
-	else if (flag & PLUS_F)
-		number[digits++] = '+';
-	else if (flag & SPACE_F)
-		number[digits++] = ' ';
 
-	for (i = 1000000000; i > 0; i /= 10)
-		if (num / i)
-			number[digits++] = ((num / i) % 10) + '0';
-	if (width > digits)
-		for (j = 0; j < (width - digits); j++)
-			buffer[buff_pos++] = ' ';
-	for (i = 0; i < digits; i++)
-		buffer[buff_pos++] = number[i];
-
-	return (digits + j);
-}
-
-/**
- * print_unsigned - prints unsigned integer
- * @list: arhument to be printed
- * @buffer: buffer
- * @buff_pos: index
- * @flag: flag
- * @length: length
- * @width: width
- * Return: no. of printed digits
- */
-int print_unsigned(va_list list, char buffer[], int buff_pos,
-		int flag, int length, int width)
-{
-	int digits = 0, j = 0, k;
-	unsigned long int num;
-	unsigned int i;
-	char number[11];
-
-	if (length == LONG)
-		num = va_arg(list, unsigned long int);
-	else if (length == SHORT)
-		num = (unsigned short)va_arg(list, int);
-	else
-		num = va_arg(list, unsigned int);
-
-	UNUSED(flag);
-	if (num == 0)
-		number[digits++] = '0';
-	for (i = 1000000000; i > 0;  i /= 10)
-		if (num / i)
-			number[digits++] = ((num / i) % 10) + '0';
-	if (width > digits)
-	{
-		for (j = 0; j < (width - digits); j++)
-			buffer[buff_pos++] = ' ';
-	}
-	for (k = 0; k < digits; k++)
-		buffer[buff_pos++] = number[k];
-	return (digits + j);
+	return (i);
 }
