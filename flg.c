89ec0e99ba5101b2e97d4e32374a59e927276e28
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 02:29:01 +0300

    Create flg.c

---
+++ flg.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * flag - select the flag
+ * @format: string
+ * @i: index
+ * @v: argument
+ *
+ * Return: number of character
+ */
+int flag(const char *format, int i, va_list v)
+{
+	int x = -1, s = 0, p = 0, h = 0;
+
+	while (format[i])
+	{
+      if (!(format[i] == ' ' || format[i] == '#' || format[i] == '+'))
+        break;
+		if (format[i] == '#')
+			h = 1;
+		if (format[i] == ' ')
+			s = 1;
+		if (format[i] == '+')
+			p = 1;
+		i++;
+	}
+	if (h == 1)
+		x = hash(format, i, v);
+	if (s == 1 && (h == 1 || p == 1))
+		s = 0;
+	if (s == 1)
+		x = space(format, i, v);
+	if (p == 1)
+		x = plus(format, i, v);
+	return (x);
+}
