158294e8d3e911db8c04f4665e8b53c0401d0196
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-28 04:47:18 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
 #include "main.h"
 
 
@@ -62,4 +64,77 @@ int _printf(const char *format, ...)
     _putchar(-1);
     va_end(arguments);
     return (count);
+}
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ * Description: _putchar uses a local buffer of 1024 to call write
+ * as little as possible
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+    int count = 0;
+    while (*str)
+    {
+        _putchar(*str++);
+        count++;
+    }
+    return (count);
+}
+
+
+
+/**
+ * convert - converts number and base into string
+ * @num: input number
+ * @base: input base
+ * @lowercase: flag if hexa values need to be lowercase
+ * Return: result string
+ */
+char *convert(unsigned long int num, int base, int lowercase)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	while (num != 0) {
+		*--ptr = rep[num % base];
+		num /= base;
+	}
+
+	return (ptr);
 }
