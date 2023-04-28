ba34ee9ccb8bce4964d02759a6fa092ec5acc311
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 02:45:12 +0300

    Update flg.c

--- flg.c
+++ flg.c
@@ -12,31 +12,30 @@
  *
  * Return: number of character
  */
+
 int flag(const char *format, int i, va_list v)
 {
 	int x = -1, s = 0, p = 0, h = 0;
 
-		while (format[i])
-		{
-	      if (!(format[i] == ' ' || format[i] == '#' || format[i] == '+'))
-		break;
+	while (format[i])
+	{
+		if (!(format[i] == ' ' || format[i] == '#' || format[i] == '+'))
+			break;
 		if (format[i] == ' ')
 			s = 1;
-			
 		if (format[i] == '#')
 			h = 1;
-		
 		if (format[i] == '+')
 			p = 1;
 		i++;
 	}
-	if (h == 1)
-		x = hash(format, i, v);
 	if (s == 1 && (h == 1 || p == 1))
 		s = 0;
-	if (s == 1)
-		x = space(format, i, v);
+	if (h == 1)
+		x = hash(format, i, v);
 	if (p == 1)
 		x = plus(format, i, v);
+	if (s == 1)
+		x = space(format, i, v);
 	return (x);
 }
