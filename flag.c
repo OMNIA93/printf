f8414fe67ed7c10d4585d7b8e4b45e43f426a459
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 02:26:00 +0300

    Update flag.c

--- flag.c
+++ flag.c
@@ -1,15 +1,18 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
 /**
  * space - print space
  * @format: string
  * @v: argument
  * @i: index
+ *
  * Return: number of character
  */
+
 int space(const char *format, int i, va_list v)
 {
 	int x = 0;
@@ -28,13 +31,38 @@ int space(const char *format, int i, va_list v)
 	va_end(copy);
 	return (x);
 }
+
+
+
+/**
+ * plus - print plus
+ * @format: string
+ * @i: index
+ * @v: argument
+ *
+ * Return: number of character
+ */
+int plus(const char *format, int i, va_list v)
+{
+	int x = 0;
+	(void)format;
+	(void)i;
+
+	x = print_int('+', v);
+	return (x);
+}
+
+
+
 /**
  * hash - print hash
  * @format: string
  * @i: index
  * @v: argument
+ *
  * Return: number of character
  */
+
 int hash(const char *format, int i, va_list v)
 {
 	int x = 0;
@@ -55,56 +83,5 @@ int hash(const char *format, int i, va_list v)
 	}
 	x = x + fun(v, format, i);
 	va_end(copy);
-	return (x);
-}
-/**
- * plus - print plus
- * @format: string
- * @i: index
- * @v: argument
- * Return: number of character
- */
-int plus(const char *format, int i, va_list v)
-{
-	int x = 0;
-	(void)format;
-	(void)i;
-
-	x = print_int('+', v);
-	return (x);
-}
-
-
-/**
- * flag - select the flag
- * @format: string
- * @i: index
- * @v: argument
- * Return: number of character
- */
-int flag(const char *format, int i, va_list v)
-{
-	int x = -1, s = 0, p = 0, h = 0;
-
-	while (format[i])
-	{
-		if (!(format[i] == ' ' || format[i] == '#' || format[i] == '+'))
-			break;
-		if (format[i] == ' ')
-			s = 1;
-		if (format[i] == '#')
-			h = 1;
-		if (format[i] == '+')
-			p = 1;
-		i++;
-	}
-	if (s == 1 && (h == 1 || p == 1))
-		s = 0;
-	if (h == 1)
-		x = hash(format, i, v);
-	if (p == 1)
-		x = plus(format, i, v);
-	if (s == 1)
-		x = space(format, i, v);
 	return (x);
 }
