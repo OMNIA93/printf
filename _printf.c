6d3da777167ea709d739c481a9bd21d5b9bb4343
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-28 04:16:32 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,111 +1,80 @@
+#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+
+
 /**
- *_printf - printf
- *@format: const char pointer
- *Description: this functions implement some functions of printf
- *Return: num of characteres printed
+ * _printf - prints a formatted string to the standard output stream
+ * @format: format string
+ *
+ * Return: number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-	const char *string;
-	int cont = 0;
-	va_list arg;
+	va_list args;
+	int count = 0;
 
-	if (!format)
-		return (-1);
+	va_start(args, format);
 
-	va_start(arg, format);
-	string = format;
-
-	cont = loop_format(arg, string);
-
-	va_end(arg);
-	return (cont);
-}
-/**
- *loop_format - loop format
- *@arg: va_list arg
- *@string: pointer from format
- *Description: This function make loop tp string pointer
- *Return: num of characteres printed
- */
-int loop_format(va_list arg, const char *string)
-{
-	int i = 0, flag = 0, cont_fm = 0, cont = 0, check_per = 0;
-
-	while (i < _strlen((char *)string) && *string != '\0')
+	while (format && *format)
 	{
-		char aux = string[i];
-
-		if (aux == '%')
+		if (*format == '%')
 		{
-			i++, flag++;
-			aux = string[i];
-			if (aux == '\0' && _strlen((char *)string) == 1)
-				return (-1);
-			if (aux == '\0')
-				return (cont);
-			if (aux == '%')
+			format++;
+
+			if (*format == 'c')
 			{
-				flag++;
-			} else
+				int c = va_arg(args, int);
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
 			{
-				cont_fm = function_manager(aux, arg);
-				if (cont_fm >= 0 && cont_fm != -1)
-				{
-					i++;
-					aux = string[i];
-					if (aux == '%')
-						flag--;
-					cont = cont + cont_fm;
-				} else if (cont_fm == -1 && aux != '\n')
-				{
-					cont += _putchar('%');
-				}
+				char *s = va_arg(args, char *);
+				fputs(s, stdout);
+				count += strlen(s);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
 			}
 		}
-		check_per = check_percent(&flag, aux);
-		cont += check_per;
-		if (check_per == 0 && aux != '\0' && aux != '%')
-			cont += _putchar(aux), i++;
-		check_per = 0;
-	}
-	return (cont);
-}
-/**
- * check_percent - call function manager
- *@flag: value by reference
- *@aux: character
- *Description: This function print % pear
- *Return: 1 if % is printed
- */
-int check_percent(int *flag, char aux)
-{
-	int tmp_flag;
-	int cont = 0;
+		else
+		{
+			putchar(*format);
+			count++;
+		}
 
-	tmp_flag = *flag;
-	if (tmp_flag == 2 && aux == '%')
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
+
+/**
+ * fputs - writes a string to the specified stream up to but not including the null character
+ * @str: string to write
+ * @stream: file pointer
+ *
+ * Return: number of characters written on success, or EOF on error
+ */
+int fputs(const char *str, FILE *stream)
+{
+	int count = 0;
+
+	if (!str || !stream)
+		return (EOF);
+
+	while (*str)
 	{
-		_putchar('%');
-		tmp_flag = 0;
-		cont = 1;
+		if (fputc(*str, stream) == EOF)
+			return (EOF);
+
+		count++;
+		str++;
 	}
-	return (cont);
-}
 
-/**
- * call_function_manager - call function manager
- *@aux: character parameter
- *@arg: va_list arg
- *Description: This function call function manager
- *Return: num of characteres printed
- */
-
-int call_function_manager(char aux, va_list arg)
-{
-	int cont = 0;
-
-	cont = function_manager(aux, arg);
-	return (cont);
+	return (count);
 }
