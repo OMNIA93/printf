b7834615996d4d2b4c0a6bf7d7f200c01cb2072a
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 03:24:14 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -81,3 +81,40 @@ long int get_number(char c, va_list v)
 		n = va_arg(v, int);
 	return (n);
 }
+
+
+/**
+ * flag - select the flag
+ * @format: string
+ * @i: index
+ * @v: argument
+ *
+ * Return: number of character
+ */
+
+int flag(const char *format, int i, va_list v)
+{
+	int x = -1, s = 0, p = 0, h = 0;
+
+	while (format[i])
+	{
+		if (!(format[i] == ' ' || format[i] == '#' || format[i] == '+'))
+			break;
+		if (format[i] == ' ')
+			s = 1;
+		if (format[i] == '#')
+			h = 1;
+		if (format[i] == '+')
+			p = 1;
+		i++;
+	}
+	if (s == 1 && (h == 1 || p == 1))
+		s = 0;
+	if (h == 1)
+		x = hash(format, i, v);
+	if (p == 1)
+		x = plus(format, i, v);
+	if (s == 1)
+		x = space(format, i, v);
+	return (x);
+}
