41f985c52b3c8196853dc7e7032a5a6b91d91fcd
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 03:03:00 +0300

    Update binary.c

--- binary.c
+++ binary.c
@@ -7,6 +7,7 @@
  * binary - convert unsigned int to binary
  * @c: character
  * @v: argument
+ *
  * Return: number of bytes
  */
 int binary(__attribute__((unused))char c, va_list v)
@@ -46,4 +47,105 @@ int binary(__attribute__((unused))char c, va_list v)
 	write(1, s, length);
 	free(s);
 	return (length);
+}
+
+/**
+ * print_hexa - hexa
+ * @c: character
+ *
+ * @v: argument
+ *
+ * Return: number of bytes
+ */
+int print_hexa(char c, va_list v)
+{
+	int i = 0, b;
+	unsigned long int n, a, num;
+	char *s;
+
+	if (c == 'l')
+		n = va_arg(v, unsigned long int);
+	else if (c == 'h')
+		n = (unsigned short)va_arg(v, int);
+	else
+		n = va_arg(v, unsigned int);
+	if (n == 0)
+		return (_putchar('0'));
+	num = n;
+	while (num)
+	{
+		i++;
+		num = num / 16;
+	}
+	s = malloc(i);
+	if (s == NULL)
+		return (0);
+	b = i - 1;
+	while (b >= 0)
+	{
+		a = n % 16;
+		if (a >= 10 && a < 16)
+		{
+			a = a % 10;
+			s[b] = 'a' + a;
+		}
+		else
+			s[b] = '0' + (n % 16);
+		n = n / 16;
+		b--;
+	}
+	write(1, s, i);
+	free(s);
+	return (i);
+}
+
+
+/**
+ * print_Hexa - hexa
+ * @c: character
+ *
+ * @v: argument
+ *
+ * Return: number of bytes
+ */
+int print_Hexa(char c, va_list v)
+{
+	int i = 0, b;
+	unsigned long int n, a, num;
+	char *s;
+
+	if (c == 'l')
+		n = va_arg(v, unsigned long int);
+	else if (c == 'h')
+		n = (unsigned short)va_arg(v, int);
+	else
+		n = va_arg(v, unsigned int);
+	if (n == 0)
+		return (_putchar('0'));
+	num = n;
+	while (num)
+	{
+		i++;
+		num = num / 16;
+	}
+	s = malloc(i);
+	if (s == NULL)
+		return (0);
+	b = i - 1;
+	while (b >= 0)
+	{
+		a = n % 16;
+		if (a >= 10 && a < 16)
+		{
+			a = a % 10;
+			s[b] = 'A' + a;
+		}
+		else
+			s[b] = '0' + (n % 16);
+		n = n / 16;
+		b--;
+	}
+	write(1, s, i);
+	free(s);
+	return (i);
 }
