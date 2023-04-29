06dead0c641c4a19bfcfdf3048d0030bd0408c68
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-29 03:12:09 +0300

    Update flg.c

--- flg.c
+++ flg.c
@@ -39,3 +39,50 @@ int flag(const char *format, int i, va_list v)
 		x = space(format, i, v);
 	return (x);
 }
+
+
+/**
+ * rot - printrot13
+ * @c: character
+ *
+ * @v: argument
+ * Return: number of bytes
+ */
+int rot(__attribute__((unused))char c, va_list v)
+{
+	int i = 0, len = 0, x = 0;
+	char *s, *p;
+
+	s = va_arg(v, char *);
+	if (s != NULL)
+	{
+		while (s[i])
+		{
+			len++;
+			i++;
+		}
+		i = 0;
+		p = malloc(len);
+		if (p != NULL)
+		{
+			while (s[i])
+			{
+				if (s[i] >= 97 && s[i] <= 109)
+					p[i] = s[i] + 13;
+				else if (s[i] >= 65 && s[i] <= 77)
+					p[i] = s[i] + 13;
+				else if (s[i] > 77)
+					p[i] = s[i] - 13;
+				i++;
+			}
+		}
+		x = write(1, p, length);
+	}
+	else
+	{
+		s = "(null)";
+		x = write(1, s, 6);
+	}
+	free(p);
+	return (x);
+}
